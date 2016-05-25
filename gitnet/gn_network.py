d4f8bbd338ba256ca80cd25936334a703dddf76c
JBWBecker <joelbecker@gto.net>
2016-05-25 16:08:19 -0400

    Weighted edge helper. Fixed TSV bug with column ignore.

--- gitnet/gn_network.py
+++ gitnet/gn_network.py
@@ -18,13 +18,14 @@ def fedits_edge(v1,v2):
     :param v2: Must be a Git log change record string (e.g. "my_dir/my_file.txt | 5 +++--") as in CommitLog "changes"
     :return:
     """
-    # Set up substitutions.
-    # For reference, see http://stackoverflow.com/questions/1450897/python-removing-characters-except-digits-from-string
-    all = string.maketrans('', '')
-    nodigs = all.translate(all, string.digits)
     # Get file name and change weight
     split_change = v2.split("|")
     fname = split_change[0].replace(" ","")
-    weight = split_change[1].translate(all,nodigs)
-
-    return (v1,fname,weight)
+    weight_str = split_change[1]
+    weight = ""
+    for char in weight_str:
+        if char.isnumeric():
+            weight += char
+    if weight == "":
+        weight = "0"
+    return (v1,fname,int(weight))
