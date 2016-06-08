f5fb02af8b33a7491bce58d626dcfac641cb46d7
John McLevey <mclevey@me.com>
2016-06-08 10:37:20 -0230

    OK i think i have it working now... wrong baseurl

--- js/main.js
+++ js/main.js
@@ -1,66 +1,111 @@
-window.onload = function() {
+// Dean Attali / Beautiful Jekyll 2016
 
-    var $menuIcon = document.getElementsByClassName('menu-icon')[0],
-        $offCanva = document.getElementsByClassName('off-canvas')[0];
-        $siteWrap = document.getElementsByClassName('site-wrapper')[0];
+var main = {
 
-    $menuIcon.addEventListener('click', function() {
-        toggleClass($menuIcon, 'close');
-        toggleClass($offCanva, 'toggled');
-        toggleClass($siteWrap, 'open');
-    }, false);
+  bigImgEl : null,
+  numImgs : null,
 
-    $menuIcon.addEventListener('mouseenter', function() {
-        addClass($menuIcon, 'hover');
-    });
-
-    $menuIcon.addEventListener('mouseleave', function() {
-        removeClass($menuIcon, 'hover');
-    });
-
-    function addClass(element, className) {
-        element.className += " " + className;
-    }
-
-    function removeClass(element, className) {
-        // Capture any surrounding space characters to prevent repeated
-        // additions and removals from leaving lots of spaces.
-        var classNameRegEx = new RegExp("\\s*" + className + "\\s*");
-        element.className = element.className.replace(classNameRegEx, " ");
-    }
-
-    function toggleClass(element, className) {
-        if (!element || !className) {
-            return;
-        }
-
-        if (element.className.indexOf(className) === -1) {
-            addClass(element, className);
+  init : function() {
+    // Shorten the navbar after scrolling a little bit down
+    $(window).scroll(function() {
+        if ($(".navbar").offset().top > 50) {
+            $(".navbar").addClass("top-nav-short");
         } else {
-            removeClass(element, className);
+            $(".navbar").removeClass("top-nav-short");
         }
-    }
-
-    // Open Twitter/share in a Pop-Up
-    var $popup = document.getElementsByClassName('popup')[0];
-    if (!$popup) {
-        return;
-    }
-    $popup.addEventListener('click', function(e) {
-        e.preventDefault()
-        var width  = 575,
-            height = 400,
-            left   = (document.documentElement.clientWidth  - width)  / 2,
-            top    = (document.documentElement.clientHeight - height) / 2,
-            url    = this.href,
-            opts   = 'status=1' +
-                     ',width='  + width  +
-                     ',height=' + height +
-                     ',top='    + top    +
-                     ',left='   + left;
-
-        window.open(url, 'twitter', opts);
-
-        return false;
     });
-}
+    
+    // On mobile, hide the avatar when expanding the navbar menu
+    $('#main-navbar').on('show.bs.collapse', function () {
+      $(".navbar").addClass("top-nav-expanded");
+    });
+    $('#main-navbar').on('hidden.bs.collapse', function () {
+      $(".navbar").removeClass("top-nav-expanded");
+    });
+  
+    // On mobile, when clicking on a multi-level navbar menu, show the child links
+    $('#main-navbar').on("click", ".navlinks-parent", function(e) {
+      var target = e.target;
+      $.each($(".navlinks-parent"), function(key, value) {
+        if (value == target) {
+          $(value).parent().toggleClass("show-children");
+        } else {
+          $(value).parent().removeClass("show-children");
+        }
+      });
+    });
+    
+    // show the big header image  
+    main.initImgs();
+  },
+  
+  initImgs : function() {
+    // If the page was large images to randomly select from, choose an image
+    if ($("#header-big-imgs").length > 0) {
+      main.bigImgEl = $("#header-big-imgs");
+      main.numImgs = main.bigImgEl.attr("data-num-img");
+
+          // 2fc73a3a967e97599c9763d05e564189
+    // set an initial image
+    var imgInfo = main.getImgInfo();
+    var src = imgInfo.src;
+    var desc = imgInfo.desc;
+      main.setImg(src, desc);
+    
+    // For better UX, prefetch the next image so that it will already be loaded when we want to show it
+      var getNextImg = function() {
+      var imgInfo = main.getImgInfo();
+      var src = imgInfo.src;
+      var desc = imgInfo.desc;      
+      
+    var prefetchImg = new Image();
+      prefetchImg.src = src;
+    // if I want to do something once the image is ready: `prefetchImg.onload = function(){}`
+    
+      setTimeout(function(){
+                  var img = $("<div></div>").addClass("big-img-transition").css("background-image", 'url(' + src + ')');
+        $(".intro-header.big-img").prepend(img);
+        setTimeout(function(){ img.css("opacity", "1"); }, 50);
+      
+      // after the animation of fading in the new image is done, prefetch the next one
+        //img.one("transitioned webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
+      setTimeout(function() {
+        main.setImg(src, desc);
+      img.remove();
+        getNextImg();
+      }, 1000); 
+        //});   
+      }, 6000);
+      };
+    
+    // If there are multiple images, cycle through them
+    if (main.numImgs > 1) {
+        getNextImg();
+    }
+    }
+  },
+  
+  getImgInfo : function() {
+    var randNum = Math.floor((Math.random() * main.numImgs) + 1);
+    var src = main.bigImgEl.attr("data-img-src-" + randNum);
+  var desc = main.bigImgEl.attr("data-img-desc-" + randNum);
+  
+  return {
+    src : src,
+    desc : desc
+  }
+  },
+  
+  setImg : function(src, desc) {
+  $(".intro-header.big-img").css("background-image", 'url(' + src + ')');
+  if (typeof desc !== typeof undefined && desc !== false) {
+    $(".img-desc").text(desc).show();
+  } else {
+    $(".img-desc").hide();  
+  }
+  }
+};
+
+// 2fc73a3a967e97599c9763d05e564189
+
+document.addEventListener('DOMContentLoaded', main.init);
